05026855acaaa46b115780d6b7982b4504baa5a9
Alline.Oliveira <Alline.Oliveira@6d2574a9-1d66-11ce-e254-3926c85b4b9b>
2010-10-09 18:07:39 +0000

    Editing file: planning/Tasks/AddTodoClass.java

--- trunk/src/sourceagile/client/planning/Tasks/AddTodoClass.java
+++ trunk/src/sourceagile/client/planning/Tasks/AddTodoClass.java
@@ -1,9 +1,12 @@
 package sourceagile.client.planning.Tasks;
 
+
+
 /** 
  * The user can insert a new task in the source code typing the name and
  description of the task in a web based form page. The system create a new
  class tagged as "To Do".
+The system can also create the test class for the new To Do Class
  * 
  * @todo 
  */ 
