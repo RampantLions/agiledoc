415b3b23281d4215886b7d430874569b8e516e4b
Alline.Oliveira <Alline.Oliveira@6d2574a9-1d66-11ce-e254-3926c85b4b9b>
2010-10-09 18:09:42 +0000

    Creating file: AddTestClass.java

---
+++ src/sourceagile/client/serverConnection/AddTestClass.java
@@ -0,0 +1,10 @@
+package sourceagile.client.serverConnection;
+
+/** 
+* Create a Test Class for the new To Do Class that is being created by the user. 
+* 
+* @todo 
+*/
+public class AddTestClass {
+
+}
