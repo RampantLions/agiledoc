2d616dd3787d5272cb1e53084d89d5f053ca8382
Alline.Oliveira <Alline.Oliveira@6d2574a9-1d66-11ce-e254-3926c85b4b9b>
2010-10-12 02:23:27 +0000

    Editing file: userFeatures/planning/projectBacklog/CreateTask.java

--- src/sourceagile/client/userFeatures/planning/projectBacklog/CreateTask.java
+++ src/sourceagile/client/userFeatures/planning/projectBacklog/CreateTask.java
@@ -2,11 +2,10 @@ package sourceagile.client.userFeatures.planning.projectBacklog;
 
 
 
+
+
 /** 
- * The user can insert a new task in the source code typing the name and
- description of the task in a web based form page. The system create a new
- class tagged as "To Do".
-The system can also create the test class for the new To Do Class
+ * The user can insert a new task in the project backlog.
  * 
  * @todo 
  */ 
