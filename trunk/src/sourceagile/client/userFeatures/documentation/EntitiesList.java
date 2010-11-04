33ddf28a61f5bcfdbaa8ba4d728dc026e21bf337
Alline.Oliveira <Alline.Oliveira@6d2574a9-1d66-11ce-e254-3926c85b4b9b>
2010-11-04 16:53:24 +0000

    Creating file: EntitiesList.java

---
+++ trunk/src/sourceagile/client/userFeatures/documentation/EntitiesList.java
@@ -0,0 +1,10 @@
+package sourceagile.client.userFeatures.documentation;
+
+/** 
+* Show a list of all classes tagged as Entity in the current project. 
+* 
+* @TODO 
+*/
+public class EntitiesList {
+
+}
