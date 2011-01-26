8bacb5b6d6f68b1e09421f1c520e91d14468aa77
Alline.Oliveira <Alline.Oliveira@6d2574a9-1d66-11ce-e254-3926c85b4b9b>
2011-01-26 23:38:27 +0000

    Creating file: ConvertProjectTest.java

---
+++ test/sourceagile/server/databaseAccess/Project/ConvertProjectTest.java
@@ -0,0 +1,33 @@
+package sourceagile.server.databaseAccess.Project;
+
+import junit.framework.TestCase;
+import sourceagile.server.databaseAccess.Project.ConvertProject;
+
+public class ConvertProjectTest extends TestCase {
+
+	public void testConvertProject() { 
+
+		// new ConvertProject();
+
+		assertTrue(true);
+	}
+
+	public void testGetProject() { 
+
+		// Project databaseProject;
+
+		// Project getProjectTested = ConvertProject.getProject(databaseProject);
+
+		// assertEquals(getProjectTested, null);
+	}
+
+	public void testGetDatabaseProject() { 
+
+		// Project project;
+
+		// Project getDatabaseProjectTested = ConvertProject.getDatabaseProject(project);
+
+		// assertEquals(getDatabaseProjectTested, null);
+	}
+
+}
