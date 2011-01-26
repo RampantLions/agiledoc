eb049ebab024897bfc43c031ea84c989faf4ede6
Alline.Oliveira <Alline.Oliveira@6d2574a9-1d66-11ce-e254-3926c85b4b9b>
2011-01-26 23:40:15 +0000

    Creating file: ListProjectsTest.java

---
+++ test/sourceagile/server/databaseAccess/Project/ListProjectsTest.java
@@ -0,0 +1,29 @@
+package sourceagile.server.databaseAccess.Project;
+
+import junit.framework.TestCase;
+import sourceagile.server.databaseAccess.Project.ListProjects;
+
+/**
+ * Test Class: ListProjects.
+ * List all the projects added in the system.
+ *
+ */
+public class ListProjectsTest extends TestCase {
+
+	public void testListProjects() { 
+
+		// new ListProjects();
+
+		assertTrue(true);
+	}
+
+	public void testList() { 
+
+		// PersistenceManager persistenceManager;
+
+		// Project> listTested = ListProjects.list(persistenceManager);
+
+		// assertEquals(listTested, null);
+	}
+
+}
