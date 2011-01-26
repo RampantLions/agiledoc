820cd3508f9093d57d09dd515b4c542d83412af1
Alline.Oliveira <Alline.Oliveira@6d2574a9-1d66-11ce-e254-3926c85b4b9b>
2011-01-26 23:41:12 +0000

    Creating file: ProjectDatabaseAccessImplementationTest.java

---
+++ trunk/test/sourceagile/server/databaseAccess/Project/ProjectDatabaseAccessImplementationTest.java
@@ -0,0 +1,51 @@
+package sourceagile.server.databaseAccess.Project;
+
+import junit.framework.TestCase;
+import sourceagile.server.databaseAccess.Project.ProjectDatabaseAccessImplementation;
+
+public class ProjectDatabaseAccessImplementationTest extends TestCase {
+
+	public void testProjectDatabaseAccessImplementation() { 
+
+		// new ProjectDatabaseAccessImplementation();
+
+		assertTrue(true);
+	}
+
+	public void testListProjects() { 
+
+		// Project> listProjectsTested = new ProjectDatabaseAccessImplementation().listProjects();
+
+		// assertEquals(listProjectsTested, null);
+	}
+
+	public void testGetProject() { 
+
+		// Long projectID;
+
+		// Project getProjectTested = new ProjectDatabaseAccessImplementation().getProject(projectID);
+
+		// assertEquals(getProjectTested, null);
+	}
+
+	public void testSaveProject() { 
+
+		// Project project;
+
+		// new ProjectDatabaseAccessImplementation().saveProject(project);
+
+		assertTrue(true);
+	}
+
+	public void testSaveProjectBacklog() { 
+
+		// Long projectID;
+
+		//  ProjectBacklog projectBacklog;
+
+		// new ProjectDatabaseAccessImplementation().saveProjectBacklog(projectID,projectBacklog);
+
+		assertTrue(true);
+	}
+
+}
