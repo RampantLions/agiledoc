654596ea399691fc990f3e89729a03dad8c4a8e9
alline.oliveira <alline.oliveira@6d2574a9-1d66-11ce-e254-3926c85b4b9b>
2011-03-02 05:02:53 +0000

    Editing file: development/client/features/FeatureDescription.java

--- src/sourceagile/development/client/features/FeatureDescription.java
+++ src/sourceagile/development/client/features/FeatureDescription.java
@@ -19,11 +19,12 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 
 
+
+
 /** 
  * Show the documentation of the class in a specification format.
- 
- TODO: Show the component and the path at the top of the description. TODO:
- Show the Taggings for the feature that is being displayed.
+
+ TODO: Show the component and the path at the top of the description.
  * 
  * @TODO
  * @MainFeature 
